6bfe8dcf040768476de315a5ee32525415542953
Shivam Patidar <125869590+shivampatidar7932@users.noreply.github.com>
2023-12-07 12:03:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-//your JS code here. If required.
-function AccioJob() {
-    alert("AccioJob");
+function printFunctionName(userInput) {
+  // Using eval to create a function dynamically with the provided name
+  const dynamicFunction = eval('(function ' + userInput + '() { alert(arguments.callee.name); })');
+
+  // Example usage
+  dynamicFunction(); // It will alert the provided function name
 }
 
-
-// Example usage
-AccioJob();
+// Example usage with user input
+var userInput = prompt("Enter a function name:");
+printFunctionName(userInput);
